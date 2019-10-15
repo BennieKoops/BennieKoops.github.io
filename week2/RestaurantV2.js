7e4c231f07e9917d8ef25a788fecf7f9618393ed
BennieKoops <56158309+BennieKoops@users.noreply.github.com>
2019-10-15 13:48:03 +0200

    updated
    code is still wet, project shelved for the moment.
    code needs to be reworked

--- week2/RestaurantV2.js
+++ week2/RestaurantV2.js
@@ -19,24 +19,131 @@ for (let D = 0; D < NDI; D++) {
 for (let D = 0; D < NFI; D++) {
     food.push (document.getElementsByClassName('food')[D].innerHTML);
     priceFood.push (document.getElementsByClassName('priceFood')[D].innerHTML);
-
 };
 
 // For loop makes strings in price array into number value for calculating prices, with thanks to Robert for help
-for (let I = 0; I < 4; I++) {
-    var TPD = parseFloat((priceDrinks[0].replace(',','.')));
+for (let I = 0; I < NDI; I++) {
+    var TP = parseFloat((priceDrinks[0].replace(',','.')));
     priceDrinks.shift();
-    priceDrinks.push(TPD);
+    priceDrinks.push(TP);
+};
+for (let I = 0; I < NFI; I++) {
+    var TP = parseFloat((priceFood[0].replace(',','.')));
+    priceFood.shift();
+    priceFood.push(TP);
 };
 
-// function to test if change of input field gets detected
-function test() {
-    alert(drinks)
+// var for defining the bestelling table
+var tbl = document.getElementById('bestelling');
+
+// function that adds cells to table row
+function addCell(tr, val, cl) {
+    var td = document.createElement('td');
+    td.innerHTML = val;
+    td.className = cl;
+    tr.appendChild(td);
+};
+
+// function that adds rows to the defined table
+function addRow(tbl, item, itemcl, aantal, prijs) {
+    
+    var tr = document.createElement('tr');
+
+    addCell(tr, item, itemcl);
+    addCell(tr, aantal, itemcl);
+    addCell(tr, "€", itemcl);
+    addCell(tr, prijs, itemcl);
+
+    tbl.appendChild(tr);
     
 };
 
-// for loop that adds the event listener to all drinks input fields on webpage. It detects when value in those fields is changed.
+// function that creates the head of the table
+function BHead(tbl) {
+    var tr = document.createElement('tr');
+    var td = document.createElement('td');
+
+    td.innerHTML = "Bestellings Bon";
+    td.id = "BBon";
+
+    tr.appendChild(td);
+    tbl.appendChild(tr);
+};
+
+// function that creates the subhead of the table for drinks
+function addHeadRowD(tbl) {
+
+    var tr = document.createElement('tr');
+    var td = document.createElement('td');
+
+    td.innerHTML = "Drinks";
+    td.id = "DBBon";
+
+    tr.appendChild(td);
+    tbl.appendChild(tr);
+};
+
+// function that creates the subhead of the table for food
+function addHeadRowF(tbl) {
+
+    var tr = document.createElement('tr');
+    var td = document.createElement('td');
+
+    td.innerHTML = "Food";
+    td.id = "FBBon";
+
+    tr.appendChild(td);
+    tbl.appendChild(tr);
+};
+
+// Drinks
+// For loop that listens to changes in the menu table to the ammount of ordered drinks
 for (let D = 0; D < NDI; D++) {
+    
+    function test6() {
+        var test3 = drinks[D].replace(/ /g,"_");
+        var test2 = document.getElementById("BBon");
+        var test4 = document.getElementById("DBBon");
+        var test5 = document.getElementsByClassName(test3);
+        console.log (test5);
+        var TP = priceDrinks[D] * aantalDrinks[D];
+
+        if (test2 === null) {
+            BHead(tbl);
+            addHeadRowD(tbl);
+            addRow(tbl, drinks[D], test3, aantalDrinks[D], TP.toFixed(2));
+        }   else if (test4 === null) {
+            addHeadRowD(tbl);
+            addRow(tbl, drinks[D], test3, aantalDrinks[D], TP.toFixed(2));
+        }   else if (test5.length === 0) {
+            addRow(tbl, drinks[D], test3, aantalDrinks[D], TP.toFixed(2));
+        }   else if (test5.length === 4) {
+            test5[1].innerHTML = aantalDrinks[D];
+            test5[3].innerHTML = TP.toFixed(2);
+        };
+    };
+
+    var DPBTN = document.getElementsByName("DPBTN")[D];
+    DPBTN.addEventListener('click', function AVD() {
+        var X = document.getElementsByName ("aantalDrinks")[D];
+        if (X.value < 15) {
+            ++X.value; 
+            aantalDrinks[D] = X.value;
+        };
+        test6();
+
+    });
+
+    var DMBTN = document.getElementsByName("DMBTN")[D];
+    DMBTN.addEventListener('click', function SVD() {
+        var X = document.getElementsByName ("aantalDrinks")[D];
+        if (X.value > 0) {
+            --X.value;
+            aantalDrinks[D] = X.value;
+        };
+        test6();
+      
+    });
     var ADL = document.getElementsByName("aantalDrinks")[D];
     ADL.addEventListener('change', function DMM() {
         var DAMM = document.getElementsByName("aantalDrinks")[D];
@@ -46,25 +153,44 @@ for (let D = 0; D < NDI; D++) {
         } else if (DAMM.value > 15) {
             DAMM.value = 15;
             alert("de maximale bestelhoeveelheid is 15");
-        }
-    });
-    var DPBTN = document.getElementsByName("DPBTN")[D];
-    DPBTN.addEventListener('click', function AVD() {
-        var X = document.getElementsByName ("aantalDrinks")[D];
-        if (X.value < 15) {
-            ++X.value;   
         };
-    });
-    var DMBTN = document.getElementsByName("DMBTN")[D];
-    DMBTN.addEventListener('click', function SVD() {
-        var X = document.getElementsByName ("aantalDrinks")[D];
-        if (X.value > 0) {
-            --X.value;
-        };
-    });
+        aantalDrinks[D] = DAMM.value;
+        test6();
+    }
+    
+    );
+    
 };
 
+// food
+// For loop that listens to changes in the menu table to the ammount of ordered drinks
 for (let D = 0; D < NFI; D++) {
+    function test7() {
+        var test3 = food[D].replace(/ /g,"_");
+        var test2 = document.getElementById("BBon");
+        var test4 = document.getElementById("FBBon");
+        var test5 = document.getElementsByClassName(test3);
+        console.log (aantalFood);
+        var TP = priceFood[D] * aantalFood[D];
+        console.log (TP);
+
+        if (test2 === null) {
+            BHead(tbl);
+            addHeadRowF(tbl);
+            addRow(tbl, food[D], test3, aantalFood[D], TP.toFixed(2));
+        }   else if (test4 === null) {
+            addHeadRowF(tbl);
+            addRow(tbl, food[D], test3, aantalFood[D], TP.toFixed(2));
+        }   else if (test5.length === 0) {
+            addRow(tbl, food[D], test3, aantalFood[D], TP.toFixed(2));
+        }   else if (test5.length === 4) {
+            test5[1].innerHTML = aantalFood[D];
+            test5[3].innerHTML = TP.toFixed(2);
+        } else if (test5[1] === 0 ) {
+            
+        };
+    };
+
     var AFL = document.getElementsByName("aantalFood")[D];
     AFL.addEventListener('change', function FMM() {
         var FAMM = document.getElementsByName("aantalFood")[D];
@@ -74,21 +200,26 @@ for (let D = 0; D < NFI; D++) {
         } else if (FAMM.value > 15) {
             FAMM.value = 15;
             alert("de maximale bestelhoeveelheid is 15");
-        }
+        };
+        test7();
     });
     var FPBTN = document.getElementsByName("FPBTN")[D];
     FPBTN.addEventListener('click', function AVF() {
         var X = document.getElementsByName ("aantalFood")[D];
         if (X.value < 15) {
-            ++X.value;   
+            ++X.value;
+            aantalFood[D] = X.value;
         };
+        test7();
     });
     var FMBTN = document.getElementsByName("FMBTN")[D];
     FMBTN.addEventListener('click', function SVF() {
         var X = document.getElementsByName ("aantalFood")[D];
         if (X.value > 0) {
             --X.value;
+            aantalFood[D] = X.value;
         };
+        test7();
     });
 };
 
